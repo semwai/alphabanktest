806c29f3dbb317708092dc68b73860d682486933
semwai <33517045+semwi@users.noreply.github.coma>
2019-06-23 01:22:56 +0300

    Готовая версия
    Добавлен функционал для:
    валидации ввода
    состояния оплаты
    
    Улучшен внешний ввид

--- alfatestback/server.js
+++ alfatestback/server.js
@@ -1,18 +1,34 @@
-const server = require('express')({})
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-server.get('/pay', (req, res) => {
+app.get('/:amount/:fio/:card/:mm/:yy/:cvv', (req, res) => {
 	res.append('Access-Control-Allow-Origin', ['*']);
 	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.append('Access-Control-Allow-Headers', 'Content-Type');
+	//res.append('Access-Control-Allow-Headers', 'Content-Type');
 	let answer = {}
 	answer.status = "success"
+	//всякие проверки на существование карты и тд...
+	try{
+		if (parseFloat(req.params.amount.replace(" ","").replace(',','.')) > 1000){
+			answer.status = "error"
+			answer.error = "Недостаточно денег"
+		}
+	}
+	catch (ex){
+		answer.status = "error"
+		answer.error = "Ошибочный ввод"
+	}
+	
+	let userData = {};
+	console.log(req.params);
 	//представим, что сервер долго обрабатывает запрос - обращается к бд, что-то умное делает и тд.
 	setTimeout(()=>{
 		res.send(JSON.stringify(answer));
 		res.status(200).end()
-	},5000);
-
-	
-	console.log(req.body);
+	},2500);
 });
-server.listen(3001);
+app.listen(3002);
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 
 import './App.css';
-import Amount from 'arui-feather/amount';
 import Form from 'arui-feather/form';
-import FormField from 'arui-feather/form-field';
 import Input from 'arui-feather/input';
 import Button from 'arui-feather/button';
 import CardInput from 'arui-feather/card-input';
 import MoneyInput from 'arui-feather/money-input';
 import Spin from 'arui-feather/spin';
- 
- 
-const AMOUNT = {
-	value: 123535,
-	currency: {
-		code: 'RUR',
-		minority: 100
-	}
-};
+import Notification from 'arui-feather/notification';
+import Select from 'arui-feather/select';
+import Icon from 'arui-feather/icon';
 
-//"красивый" эффект при перемещении мыши вам обеспечен!
+//"красивый" эффект при перемещении мыши вам обеспечен! 
 window.onload = () => {
 	var sde = document.querySelectorAll('.Card');
 	document.onmousemove = (mouse => {
@@ -31,81 +23,175 @@ window.onload = () => {
 	})
 }
 
-
-
+//массивы для полей ввода месяца и года окончания работы карты
+const MM = [
+    { value: '1', text: 'Январь' },
+    { value: '2', text: 'Февраль' },
+	{ value: '3', text: 'Март' },
+	{ value: '4', text: 'Апрель' },
+	{ value: '5', text: 'Май' },
+	{ value: '6', text: 'Июнь' },
+	{ value: '7', text: 'Июль' },
+	{ value: '8', text: 'Август' },
+	{ value: '9', text: 'Сентябрь' },
+	{ value: '10', text: 'Октябрь' },
+	{ value: '11', text: 'Ноябрь' },
+	{ value: '12', text: 'Декабрь' },
+];
+const YY = [
+    { value: '19', text: '2019' },
+    { value: '20', text: '2020' },
+	{ value: '21', text: '2021' },
+	{ value: '22', text: '2022' },
+	{ value: '23', text: '2023' },
+];
 class App extends React.Component  {
+	
 	constructor(props) {
 		super(props);
-
+		 
 		this.state = {
 			/*состояние выплаты
 			0 - ждем заполнения данных польователя
 			1 - ждем обработки сообщения сервером
-			2 - успешная выплата
-			3 - ошибка выплаты
+			2 - конечное сообщение
 			*/
 			progress: 0,
+			message:"",
+			errnotvisible: false,
+			//флаги корректного ввода
+			CVVflag:false,
+			nameValidateFlag:false,
+			cardValidateFlag:false,
+			moneyValidateFlag:false
 		};
 	}
 	makeQuery(f) {
 		let out = {};
-		out.num = f.target[0].value;
-		out.mm = f.target[1].value;
-		out.yy = f.target[2].value;
-		out.fio = f.target[3].value;
-		out.cvv = f.target[4].value;
-		if (out.cvv.length !== 3 ||
+		out.summ =  f.target[0].value.replace(/ /g,'')
+		out.num = f.target[1].value.replace(/ /g,'')
+		out.mm = f.target[2].value;
+		out.yy = f.target[4].value;
+		out.fio = f.target[8].value;
+		out.cvv = f.target[7].value;
+		if (out.summ <= 0 ||
+			out.cvv.length !== 3 ||
 			out.fio.length === 0 ||
 			out.mm.length === 0 ||
 			out.yy.length === 0 ||
 			out.num.length === 0) {
-			alert("Неверно введеные данные");
-			return;
+			this.setState({errnotvisible:true});
+			
 		}
 		var object = this;
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://localhost:3001/pay', true);
-		xhr.send(JSON.stringify(out));
+		xhr.open('GET', `http://localhost:3002/${out.summ}/${out.fio}/${out.num}/${out.mm}/${out.yy}/${out.cvv}/`, true);
+		console.log(out);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send();
+		
 		object.setState({ progress: 1 })
+		
 		xhr.onreadystatechange = function () {
 			if (this.readyState != 4) return;
 			if (this.status != 200) {
 				// обработать ошибку
-				alert('ошибка: ' + (this.status ? this.statusText : 'запрос не удался'));
+				object.setState({ progress: 2,message: "Сервер не отвечает 😥" })
 				return;
 			}
-			let answer = JSON.parse(xhr.responseText);
-			console.log(answer);
-			if (answer.status == "success"){
-				console.log(object)
-				object.setState({ progress: 2 })
+			try{
+				let answer = JSON.parse(xhr.responseText);
+				console.log(answer);
+				if (answer.status == "success"){
+					object.setState({ progress: 2,message: "Выплата прошла успешно!" })
+				} else{
+					object.setState({ progress: 2,message: answer.error })
+				}
 			}
+			catch (ex){
+				object.setState({ progress: 2,message: "Ошибка работы сервера 😥" })
+			}
+			
 				
 		}
 	}
+	validateCVV(value){
+		this.setState({CVVflag:value.length==3})
+	}
+	validateMoney(value){
+		this.setState({moneyValidateFlag:value.length>0})
+	}
+	validateCard(value){
+		this.setState({cardValidateFlag:value.length>=16})
+	}
+	validateName(value){
+		var flag = value.length>0;
+		value.split('').forEach(e=>{
+			if (e==' ')
+				{}
+			else
+				if (e<'A'||e>'Z')
+					flag = false;
+		});
+		this.setState({nameValidateFlag:flag})
+	}
+	resetValidateFlags(){
+		this.setState({CVVflag:false,
+			nameValidateFlag:false,
+			cardValidateFlag:false,
+			moneyValidateFlag:false})
+	}
 	render(){
-		 
 		switch (this.state.progress){
 			default:
 				return (
 					<div className="App">
+					<Notification
+						visible={ this.state.errnotvisible }
+						status='error'
+						autoCloseDelay="1"
+						offset={ 100 }
+						stickTo='right'
+						title='Введите все данные!'
+						onCloseTimeout={ () => { this.setState({ errnotvisible: false }); } }
+						onCloserClick={ () => { this.setState({ errnotvisible: false }); } }
+					>
+						Не хватает кое-чего
+					</Notification>
 						<header className="App-header">
 							<Form onSubmit={(f) => { this.makeQuery(f) }}>
-								<Amount size="l" amount={AMOUNT} isHeading={true} />
+							Сумма:<MoneyInput
+								showCurrency={ true }
+								currencyCode='RUR'
+								bold={ true }
+								size="xl"
+								onChange={(f)=>{this.validateMoney(f)}}
+								error={!this.state.moneyValidateFlag} 
+							/>
 								< div className="Card" >
-									<p><CardInput className="custom__input" value="1111 1111 1111 1111 11" size="l" label='Номер карты' /></p>
 									<p>
-										<Input value="11" placeholder="MM" className="Picker" placeholder="ММ" size="m" mask="11"></Input>
-										<Input value="11" placeholder="YY" className="Picker" placeholder="ГГ" size="m" mask="11"></Input>
+									<CardInput className="custom__input"  size="l" label='Номер карты' 
+									onChange={(f)=>{this.validateCard(f)}}
+									error={!this.state.cardValidateFlag}/></p>
+									<p>
+									<Select className="Picker" size="m" mode='radio' options={ MM }/>
+									<Select className="Picker" size="m" mode='radio' options={ YY }/>
 									</p>
-									<p><Input className="custom__input" value="Попов Иван" placeholder='ФИО' size="m" ></Input></p>
+									<p><Input className="custom__input" placeholder='ФИО' size="m" 
+									error={!this.state.nameValidateFlag} 
+									onChange={(f)=>{this.validateName(f)}}
+									rightAddons={this.state.nameValidateFlag?"":"Большие латинские буквы"}></Input></p>
 								</div>
 								< div className="Card" >
 									<br></br>
 									<hr className="Black-field"></hr>
-									<Input className="CVV" label='CVV' size="l" value="111" mask="111"></Input>
+									<Input className="CVV" label='CVV' 
+									onChange={(f)=>{this.validateCVV(f)}} size="l" mask="111"
+									error={!this.state.CVVflag} ></Input>
 								</div>
-								<Button view='extra' type='submit'>Оплатить</Button>
+								<Button view='extra' type='submit' 
+								 disabled={!(this.state.CVVflag&&this.state.nameValidateFlag&&this.state.cardValidateFlag&&this.state.moneyValidateFlag) }
+								 >Оплатить</Button>
 							</Form>
 						</header>
 					</div>
@@ -124,20 +210,13 @@ class App extends React.Component  {
 				return (
 					<div className="App">
 						<header className="App-header">
-							<p>Выплата успешна!</p>
+							<p>{this.state.message}</p>
+							<Button onClick={()=>{this.setState({progress:0});this.resetValidateFlags()}} >Назад</Button>
 						</header>
+						
 					</div>
 				)
 			break;
-			case 3:
-				return (
-					<div className="App">
-						<header className="App-header">
-							<p>Выплата не прошла :C</p>
-						</header>
-					</div>
-				)
-				break;
 		}
 	}
 }
